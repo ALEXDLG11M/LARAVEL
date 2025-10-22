9f1ec7ae1d29febd7a64bcd882e11d9399853002
Quiji <quiji33@gmail.com>
2025-10-22 01:50:17 -0600

    feat(questions): controlador con store/show/edit/update/destroy

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use App\Http\Requests\StoreQuestionRequest;
+use App\Http\Requests\UpdateQuestionRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+
+class QuestionController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['show']);
+    }
+
+    public function store(StoreQuestionRequest $request): RedirectResponse
+    {
+        $question = Question::create([
+            'title'   => $request->input('title'),
+            'body'    => $request->input('body'),
+            'user_id' => $request->user()->id,
+        ]);
+
+        return redirect()
+            ->route('questions.show', $question)
+            ->with('status', 'Pregunta creada correctamente.');
+    }
+
+    public function show(Question $question)
+    {
+        return view('questions.show', compact('question'));
+    }
+
+    public function edit(Question $question)
+    {
+        $this->authorize('update', $question);
+        return view('questions.edit', compact('question'));
+    }
+
+    public function update(UpdateQuestionRequest $request, Question $question): RedirectResponse
+    {
+        $this->authorize('update', $question);
+        $question->update($request->only(['title','body']));
+        return redirect()
+            ->route('questions.show', $question)
+            ->with('status', 'Pregunta actualizada correctamente.');
+    }
+
+    public function destroy(Request $request, Question $question): RedirectResponse
+    {
+        $this->authorize('delete', $question);
+        $question->delete();
+        return redirect()->route('home')->with('status', 'Pregunta eliminada correctamente.');
+    }
+}
