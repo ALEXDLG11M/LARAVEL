173442ca03b5125fe16f1908cb372d253e6df916
Quiji <quiji33@gmail.com>
2025-10-22 01:54:34 -0600

    feat(routes): rutas de questions con middleware auth salvo show

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,20 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuestionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::post('/questions', [QuestionController::class, 'store'])->name('questions.store');
+    Route::get('/questions/{question}/edit', [QuestionController::class, 'edit'])->name('questions.edit');
+    Route::match(['put','patch'], '/questions/{question}', [QuestionController::class, 'update'])->name('questions.update');
+    Route::delete('/questions/{question}', [QuestionController::class, 'destroy'])->name('questions.destroy');
+});
+
+Route::get('/questions/{question}', [QuestionController::class, 'show'])->name('questions.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
